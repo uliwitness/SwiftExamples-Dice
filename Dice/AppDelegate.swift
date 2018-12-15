8cb2596d51b7c8bb093440bbe5e5befa7565891f
Not Kitteh <notakitteh@supermodel.local>
2018-12-15 19:38:54 +0100

    Initial Commit

---
+++ Dice/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Dice
+//
+//  Created by Not A Kitteh on 15.12.18.
+//  Copyright © 2018 Not A Kitteh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
